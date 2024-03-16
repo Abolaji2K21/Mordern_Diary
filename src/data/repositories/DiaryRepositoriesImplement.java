784f18dddef15f2c827e433b33b16d0f4c4a6e3a
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-16 14:21:24 +0100

    updateDiaryImplementation

--- src/data/repositories/DiaryRepositoriesImplement.java
+++ src/data/repositories/DiaryRepositoriesImplement.java
@@ -9,36 +9,67 @@ public class DiaryRepositoriesImplement implements DiaryRepositories{
 
 
     private List<Diary> diaries = new ArrayList<>();
+    private int currentDiaryId = 0;
+
+
+    private int getCurrentDiaryId(){
+        return currentDiaryId += 1;
+    }
+
 
     public Diary save(Diary diary) {
-        return null;
+        if (isNew(diary)) addIdTo(diary);
+        else update(diary);
+        diaries.add(diary);
+        return diary;
+    }
+
+    private void update(Diary diary) {
+        for (int checkIndex = 0; checkIndex < diaries.size(); checkIndex++)
+            if (diaries.get(checkIndex).getId() == diary.getId()){
+                diaries.set(checkIndex, diary);
+                break;
+            }
+    }
+
+    private void addIdTo(Diary diary) {
+        diary.setId(getCurrentDiaryId());
+    }
+
+    private boolean isNew(Diary diary) {
+        return diary.getId() == 0;
     }
 
 
     @Override
     public List<Diary> findAll() {
-        return null;
+        return diaries;
     }
 
     @Override
     public Diary findById(String username) {
+        for (Diary myDiary : diaries)
+            if (myDiary.getUsername().equals(username))
+                return myDiary;
         return null;
     }
 
     @Override
     public long count() {
-        return 0;
+        return diaries.size();
     }
-
 
 
     @Override
     public void delete(String username) {
-
+        Diary myDiary = findById(username);
+//        for (Diary myDiary : diaries)
+//            if (myDiary.getUsername().equals(username))
+                diaries.remove(myDiary);
     }
 
     @Override
     public void delete(Diary diary) {
-
+        diaries.remove(diary);
     }
 }
